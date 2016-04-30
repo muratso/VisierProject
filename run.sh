fbfea31cd89a1bb49105e9b7590a6833eb48068a
Muratso <rockstar2304@gmail.com>
2016-04-30 00:13:20 -0300

    Add: Docker build at run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 apt-get install ansible -y
 ansible-playbook -i ./ansible/playbook/hosts ./ansible/playbook/playbook.yml
+docker build -t="jenkins" ./docker/jenkins
+docker build -t="protractor" ./docker/protractor
